cb865c56165776792ba6e3484d90907769ef37d4
Misha <misha.murzin.mm@gmail.com>
2022-07-15 10:29:11 +0500

    Update proxy_tester.py
    исправление ошибок
    
    Co-Authored-By: Redume <49362257+Redume@users.noreply.github.com>

--- proxy_tester.py
+++ proxy_tester.py
@@ -1,42 +1,42 @@
 import os, time, json, requests
 
 try:
-	import requests
+    import requests
 except:
-	print("Установка модклей...")
-	os.system("pip install -r requirements.txt")
-	print("Запустите файл снова!")
-	exit()
+    print("Установка модклей...")
+    os.system("pip install -r requirements.txt")
+    print("Запустите файл снова!")
+    exit()
 
 with open("proxies.json") as p:
-	proxies = json.load(p)["proxies"]
+    proxies = json.load(p)["proxies"]
 p.close()
 
 for proxy in proxies:
-	r = requests.get(f"http://ip-api.com/json/")
-	real_ip = json.loads(r.text)["query"]
-	protocol = proxy.split("://")[0]
+    r = requests.get(f"http://ip-api.com/json/")
+    real_ip = json.loads(r.text)["query"]
+    protocol = proxy.split("://")[0]
 
-	p = {
-		protocol: proxy
-	}
+    p = {
+        protocol: proxy
+    }
 
-	try:
-		t1 = time.time()
-		r = requests.get(f"http://ip-api.com/json/", proxies=p)
-		data = json.loads(r.text)
-		t2 = time.time()
-		speed = t2 - t1
-	except requests.exceptions.ProxyError as err:
-		print(f"{proxy} : не удалось подключится")
-		continue
-	except requests.exceptions.ConnectTimeout as err:
-		print(f"{proxy} : не удалось подключится")
-		continue
+    try:
+        t1 = time.time()
+        r = requests.get(f"http://ip-api.com/json/", proxies=p)
+        data = json.loads(r.text)
+        t2 = time.time()
+        speed = t2 - t1
+    except requests.exceptions.ProxyError as err:
+        print(f"{proxy} : не удалось подключиться")
+        continue
+    except requests.exceptions.ConnectTimeout as err:
+        print(f"{proxy} : не удалось подключиться")
+        continue
 
-	if real_ip != data["query"]:
-		print(f"{proxy} : работет!\nSpeed: {round(speed, 3)}s\n")
-	else:
-		print(f"{proxy} : ip не изменился\n")
+    if real_ip != data["query"]:
+        print(f"{proxy} : работет!\nSpeed: {round(speed, 3)}s\n")
+    else:
+        print(f"{proxy} : ip не изменился\n")
 
 input("Press Enter to exit...")
